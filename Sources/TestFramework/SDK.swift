e2340aa414eb96aee5bb1eb0ca3e18bc8abb9787
javlonbekdev <javlonbekdev@gmail.com>
2025-08-03 12:04:28 +0500

    first commit

---
+++ Sources/TestFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  TestFramework
+//
+//  Created by Javlonbek Dev on 03/08/25.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work ...")
+    }
+}
