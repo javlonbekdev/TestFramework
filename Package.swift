f36070ab0632bc5a7447f664154e2dcd392ac816
javlonbekdev <javlonbekdev@gmail.com>
2025-08-04 07:30:52 +0500

    bug fixed

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .binaryTarget(
-                name: "TestFrameworkSDK",
-                path: "./Sources/TestFrameworkSDK.xcframework")
+                name: "TestFramework",
+                path: "./Sources/TestFramework.xcframework")
         ]
 )
